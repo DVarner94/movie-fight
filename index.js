8e2c16d6d1f8ce7ccc5bf0374598fdedb32a8b01
Dominic Varner <dommy@MLBs-Lappy.local>
2020-09-13 20:50:47 -0700

    fetching data, using axios, omdbapi.com, then tested

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '30da1ff6',
+            s: 'avengers'
+        }
+    });
+    console.log(response.data);
+}
+
+fetchData();
