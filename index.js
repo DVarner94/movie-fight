adc66e4a50a3c4749f7f81e9fedb1d6a8175a3cd
Dominic Varner <dommy@MLBs-Lappy.local>
2020-09-13 22:13:06 -0700

    added 'debounce' function then moved to utils.js

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ const fetchData = async (searchTerm) => {
         }
     });
     console.log(response.data);
-}
+};
 
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = (event) => {
-    if (timeoutId){
-        clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {fetchData(event.target.value)
-    },500)
-    
-};
 
-input.addEventListener('input',onInput);
+const onInput = event => {
+    fetchData(event.target.value)
+};
+input.addEventListener('input', debounce(onInput, 500));
